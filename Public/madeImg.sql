84e43dee1b8c20681ff687d52b81320c33d55d6b
jianke <jianke@jiihome.com>
2018-04-28 12:40:10 +0800

    Update madeImg.sql

--- Public/madeImg.sql
+++ Public/madeImg.sql
@@ -6,3 +6,6 @@ CREATE TABLE `made_goods_img` (
   PRIMARY KEY (`id`),
   KEY `goods_id` (`goods_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='商品描述轮播图';
+
+INSERT INTO `made_admin` (`id`, `username`, `password`) VALUES
+(1, 'root', '48601017c9c217061bc9c231f246ca7f');
